b59bd663747925ff123b848ba63855939b6160ac
azuredragon3000 <phanhuycuong2909@gmail.com>
2022-06-13 16:54:56 +0700

    lib comment

--- app/src/main/java/com/myapp/example3/MainActivity.java
+++ app/src/main/java/com/myapp/example3/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.myapp.mylibrary.MyLib;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyLib hello = new MyLib("a","b");
     }
 }

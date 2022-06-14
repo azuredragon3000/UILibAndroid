497e51c34ad6c598488a29ec8b11a1493382b419
Huy Cuong <phanhuycuong2909@gmail.com>
2022-06-14 09:56:07 +0700

    commmit

---
+++ mylibrary/src/main/java/com/myapp/mylibrary/CustomButton1.java
@@ -0,0 +1,38 @@
+package com.myapp.mylibrary;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class CustomButton1 extends androidx.appcompat.widget.AppCompatButton {
+
+    public static void customView(View v, int backgroundColor, int borderColor) {
+        GradientDrawable shape = new GradientDrawable();
+        shape.setShape(GradientDrawable.RECTANGLE);
+        shape.setCornerRadii(new float[] { 8, 8, 8, 8, 0, 0, 0, 0 });
+        shape.setColor(backgroundColor);
+        shape.setStroke(3, borderColor);
+        v.setBackground(shape);
+    }
+
+    public CustomButton1(@NonNull Context context) {
+        super(context);
+        int color1 = Color.parseColor("#AA66CC");
+        int color2 = Color.parseColor("#D3D3D3");
+        customView(this,color1,color2);
+    }
+
+    public CustomButton1(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomButton1(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}

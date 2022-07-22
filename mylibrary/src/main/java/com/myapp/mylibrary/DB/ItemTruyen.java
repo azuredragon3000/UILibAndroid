f8ceb7a1fa0e9ec80e9e33b5e0996232b444d93f
Huy Cuong <phanhuycuong2909@gmail.com>
2022-07-22 11:32:13 +0700

    commit 22/7/2022

--- mylibrary/src/main/java/com/myapp/mylibrary/DB/ItemTruyen.java
+++ mylibrary/src/main/java/com/myapp/mylibrary/DB/ItemTruyen.java
@@ -3,7 +3,7 @@ package com.myapp.mylibrary.DB;
 public class ItemTruyen {
     String src;
     String txt;
-    ItemTruyen(String txt , String src){
+    public ItemTruyen(String txt , String src){
         this.src = src;
         this.txt = txt;
     }

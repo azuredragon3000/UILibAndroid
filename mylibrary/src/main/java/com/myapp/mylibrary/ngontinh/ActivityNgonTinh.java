3574b441ebbb639384d0101b81e92fbad82ad58a
Huy Cuong <phanhuycuong2909@gmail.com>
2022-08-31 15:10:31 +0700

    commit

--- mylibrary/src/main/java/com/myapp/mylibrary/ngontinh/ActivityNgonTinh.java
+++ mylibrary/src/main/java/com/myapp/mylibrary/ngontinh/ActivityNgonTinh.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 
+import com.myapp.mylibrary.DB.DatabaseNgonTinh;
 import com.myapp.mylibrary.boitinhyeu.ModelDanhNgon;
 import com.myapp.mylibrary.databinding.ActivityNgontinhBinding;
 
@@ -16,12 +17,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public abstract class ActivityNgonTinh extends AppCompatActivity {
+public class ActivityNgonTinh extends AppCompatActivity {
 
     List<ModelDanhNgon> modelDanhNgonList = new ArrayList<>();
     private final Handler mHandler = new Handler();
     List<ModelDanhNgon> ngonList = new ArrayList<>();
     ActivityNgontinhBinding binding;
+
+    public  String DB_PATH= "/data/data/com.myapp.mylibrary/databases/";
+    public  String DATABASE_BOIPHUONGDONG = "datatonghop.db";
+    public  String DATABASE_NAMSINH = "tuvitrondoi.sqlite";
+    public  String DATABASE_BOITINHYEUPD = "boiphuongdong_bty.db";
+    public  String DATABASE_BOITINHYEUCHD = "cunghoangdao.db";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +37,9 @@ public abstract class ActivityNgonTinh extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        modelDanhNgonList = getDanhNgon();//((SubApp) getApplication()).getDatabaseNgonTinh().getDanhNgon();
-
+        DatabaseNgonTinh databaseNgonTinh = DatabaseNgonTinh.getInstance(getApplication(),DB_PATH,DATABASE_BOIPHUONGDONG);
+        //modelDanhNgonList = getDanhNgon();//((SubApp) getApplication()).getDatabaseNgonTinh().getDanhNgon();
+        modelDanhNgonList = databaseNgonTinh.getDanhNgon();
         LinearLayoutManager manager = new LinearLayoutManager(this);
         binding.danhNgonRclView.setLayoutManager(manager);
         binding.danhNgonRclView.setHasFixedSize(true);
@@ -39,6 +47,7 @@ public abstract class ActivityNgonTinh extends AppCompatActivity {
         loadData(0, 100);
         final AdapterDanhNgon adapter = new AdapterDanhNgon(this, ngonList, binding.danhNgonRclView);
         binding.danhNgonRclView.setAdapter(adapter);
+
 
         adapter.setOnLoadMoreListener(() -> {
             if (ngonList.size() <= modelDanhNgonList.size()) {
@@ -50,7 +59,6 @@ public abstract class ActivityNgonTinh extends AppCompatActivity {
                     int start = ngonList.size();
                     int end = start + 100;
                     loadData(start, end);
-
                     adapter.setLoaded();
                 }, 1000);
             } else {
@@ -59,14 +67,13 @@ public abstract class ActivityNgonTinh extends AppCompatActivity {
         });
     }
 
-    protected abstract List<ModelDanhNgon> getDanhNgon();
+    //protected List<ModelDanhNgon> getDanhNgon();
 
     private void loadData(int start, int end) {
         for (int i = start; i < end; i++) {
             ngonList.add(new ModelDanhNgon(modelDanhNgonList.get(i).getId(),
                     modelDanhNgonList.get(i).getContent(), modelDanhNgonList.get(i).getAuthor()));
         }
-
     }
 
     @Override
